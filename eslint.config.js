521f8c423dc0aaaed8e37c9a66dcd9d03a81258c
pipapaparapo2024 <bedzanansergej5110@gmail.com>
2025-09-03 17:36:36 +0300

    all right rilваыав

--- eslint.config.js
+++ eslint.config.js
@@ -2,26 +2,27 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
-export default [
-  // Игнорируемые папки
-  { ignores: ["dist/", "node_modules/"] },
-
-  // Основной конфиг для TS/TSX
+export default tseslint.config(
+  {
+    ignores: ["dist/", "node_modules/"]
+  },
   {
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       sourceType: "module",
       globals: globals.browser,
-      parser: "@typescript-eslint/parser",
+      parser: tseslint.parser,
     },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
-      "react-hooks/exhaustive-deps": "warn",
+      // Только те правила, которые ты хочешь
+      "react-hooks/exhaustive-deps": "warn", // ← мы сами контролируем
       "react-refresh/only-export-components": "warn",
 
       "no-unused-vars": "off",
@@ -34,5 +35,5 @@ export default [
         },
       ],
     },
-  },
-];
+  }
+);
