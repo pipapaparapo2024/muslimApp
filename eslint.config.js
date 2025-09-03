1255ad9bfc367c1c138fbe98f15dccf562f3261d
pipapaparapo2024 <bedzanansergej5110@gmail.com>
2025-09-03 17:33:42 +0300

    all right rilваыав

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,16 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import { FlatCompat } from "@typescript-eslint/eslint-plugin";
+
+// Создаём совместимый конфиг для старых extends
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
 
 export default [
   // Игнорируемые папки
   { ignores: ["dist/", "node_modules/"] },
-
-  // Базовый конфиг JavaScript
-  js.configs.recommended,
 
   // Основной конфиг для TS/TSX
   {
@@ -18,28 +19,17 @@ export default [
     languageOptions: {
       ecmaVersion: 2020,
       sourceType: "module",
-      globals: {
-        ...globals.browser,
-      },
-      parser: tsParser,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
+      globals: globals.browser,
+      parser: "@typescript-eslint/parser",
     },
     plugins: {
-      "@typescript-eslint": tseslint,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
-      ...tseslint.configs.recommended.rules,
       "react-hooks/exhaustive-deps": "warn",
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      "react-refresh/only-export-components": "warn",
+
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": [
         "error",
