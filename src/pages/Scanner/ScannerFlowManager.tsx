8c0542ceb39a13ea7380e5bc27879d57413280e0
pipapaparapo2024 <bedzanansergej5110@gmail.com>
2025-09-14 14:50:11 +0300

    почти готово

--- src/pages/Scanner/ScannerFlowManager.tsx
+++ src/pages/Scanner/ScannerFlowManager.tsx
@@ -12,16 +12,20 @@ export const ScannerFlowManager: React.FC = () => {
     // Если есть результат сканирования и загрузка завершена
     if (scanResult && !isLoading) {
       console.log("Scan result received:", scanResult);
+      console.log("scanResult.status", scanResult.status);
+      console.log("ProductStatus.NEEDS_INFO", ProductStatus.NEEDS_INFO);
+      console.log("scanResult.id", scanResult.id);
+      
       if (scanResult.status === ProductStatus.NEEDS_INFO) {
-        console.log("/scanner/notScanned scanResult.status")
+        console.log("/scanner/notScanned scanResult.status");
         navigate("/scanner/notScanned");
       } else if (scanResult.id) {
-        console.log("/scanner/notScanned scanResult.id")
-        sessionStorage.setItem('lastScanId', scanResult.id);
+        console.log("/scanner/notScanned scanResult.id");
+        sessionStorage.setItem("lastScanId", scanResult.id);
         navigate(`/scanner/historyScanner/${scanResult.id}`);
       }
     }
-    
+
     // Если есть ошибка и загрузка завершена
     if (error && !isLoading) {
       console.log("Error occurred:", error);
@@ -30,7 +34,7 @@ export const ScannerFlowManager: React.FC = () => {
   }, [scanResult, error, isLoading, navigate]);
 
   if (isLoading || showAnalyzing) {
-    return <AnalyzingIngredient key={Date.now()}/>;
+    return <AnalyzingIngredient key={Date.now()} />;
   }
 
   return null;
