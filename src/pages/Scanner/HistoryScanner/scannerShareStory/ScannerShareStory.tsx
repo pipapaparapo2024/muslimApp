d652b9b19cfc07144a28f7ad4c76b6937960cab3
pipapaparapo2024 <bedzanansergej5110@gmail.com>
2025-09-23 15:17:21 +0300

    почти готово

--- src/pages/Scanner/HistoryScanner/scannerShareStory/ScannerShareStory.tsx
+++ src/pages/Scanner/HistoryScanner/scannerShareStory/ScannerShareStory.tsx
@@ -100,7 +100,6 @@ export const ScannerShareStory: React.FC = () => {
   return (
     <PageWrapper
       showBackButton={true}
-      styleHave={false}
       navigateTo="/scanner/historyScanner"
     >
       <div className={styles.container}>
