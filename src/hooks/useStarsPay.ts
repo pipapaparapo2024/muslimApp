7da9234375ef6b33fb874eb6ca7ded3eb4fe360e
pipapaparapo2024 <bedzanansergej5110@gmail.com>
2025-09-30 15:51:01 +0300

    почти готово

--- src/hooks/useStarsPay.ts
+++ src/hooks/useStarsPay.ts
@@ -18,6 +18,7 @@ export const useStarsPay = () => {
   ): Promise<StarsPaymentResponse> => {
     try {
       console.log("params.productId",params.productId)
+      console.log("fffffffffffffffffffffffff")
       const response = await quranApi.post("/api/v1/payments/stars/invoice", {
         productId: params.productId, 
       });
