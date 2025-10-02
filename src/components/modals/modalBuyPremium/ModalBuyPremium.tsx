b457119057be796304b06abe6616d43cf74c1ee5
pipapaparapo2024 <bedzanansergej5110@gmail.com>
2025-10-02 16:28:49 +0300

    почти готово

--- src/components/modals/modalBuyPremium/ModalBuyPremium.tsx
+++ src/components/modals/modalBuyPremium/ModalBuyPremium.tsx
@@ -44,11 +44,12 @@ export const BuyPremiumModal: React.FC<BuyPremiumModalProps> = ({
     return premiumProducts
       .map((product) => {
         const days = product.revardAmount;
-        let label = product.title;
+        // let label = product.title;
+        let label;
         if (days === 7) label = `1 ${t("week")}`;
-        // else if (days === 30) label = `1 ${t("month")}`;
-        // else if (days === 365) label = `1 ${t("year")}`;
-        // else label = `${days} ${t("days")}`;
+        else if (days === 30) label = `1 ${t("month")}`;
+        else if (days === 365) label = `1 ${t("year")}`;
+        else label = `${days} ${t("days")}`;
 
         return {
           label,
