d98e98617bfa57631ce8a20685d8ee4b56e7f1b1
pipapaparapo2024 <bedzanansergej5110@gmail.com>
2025-10-10 16:46:23 +0300

    islamapp

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react(), svgr()],
@@ -13,8 +14,21 @@ export default defineConfig({
   define: {
     "process.env": {},
   },
-   css: {
-    postcss: './postcss.config.js'
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: [
+            '> 1%',
+            'last 2 versions', 
+            'not dead',
+            'iOS >= 9',
+            'Android >= 4.4',
+            'Chrome >= 49'
+          ]
+        })
+      ]
+    }
   },
   server: {
     host: true,
