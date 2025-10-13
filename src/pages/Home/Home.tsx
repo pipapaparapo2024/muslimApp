ae64782408c226dd70d93ae8980f3f72084c6f7a
pipapaparapo2024 <bedzanansergej5110@gmail.com>
2025-10-13 20:47:47 +0300

    islamapp

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -57,7 +57,7 @@ export const Home: React.FC = () => {
   return (
     <PageWrapper>
       <Header />
-      ываыаываываыава
+      ываыаываываыав
       {isLoading && (
         <div className={styles.loadingContainer}>
           <LoadingSpinner />
