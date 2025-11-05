4df3aa91c1a9fe5cb60d45f1f62badd4bc83244e
pipapaparapo2024 <bedzanansergej5110@gmail.com>
2025-11-05 17:30:28 +0300

    app ready

--- src/hooks/useGeoStore.ts
+++ src/hooks/useGeoStore.ts
@@ -1,7 +1,6 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import axios from "axios";
-import { isErrorWithMessage } from "../api/api";
 
 export interface IpData {
   country_name: string;
